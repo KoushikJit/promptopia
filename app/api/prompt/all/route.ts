bf1201bc208dcdb3204e1ab989edde5004d937ba
Koushik Sarkar <koushikjit@gmail.com>
2023-10-29 11:27:45 +0530

    logs for all posts

--- app/api/prompt/all/route.ts
+++ app/api/prompt/all/route.ts
@@ -5,7 +5,7 @@ export async function GET(req: Request, res: Response) {
   console.log("GETTING prompts list for all");
   const queryDBRes = await queryDB();
   const posts = await queryDBRes.json();
-  console.log("got posts: " + JSON.stringify(queryDBRes));
+  console.log("got posts: " + JSON.stringify(posts));
   console.log(posts);
   return new Response(JSON.stringify(posts), { status: 200 });
 }
@@ -13,7 +13,7 @@ export async function GET(req: Request, res: Response) {
 async function queryDB(): Promise<Response> {
   try {
     await connectToDB();
-
+    console.log("querying db for all posts")
     const posts = await PostModel.find({}).populate("user");
     return new Response(JSON.stringify(posts), { status: 200 });
   } catch (error) {
